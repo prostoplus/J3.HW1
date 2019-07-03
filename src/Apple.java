e6cee4d68d223bef0ccf33bcf71c9a9822b319b3
Vladimir <larsoni4@rambler.ru>
2019-07-03 12:07:13 +0300

    Work finished

---
+++ src/Apple.java
@@ -0,0 +1,6 @@
+public class Apple extends Fruit{
+
+    public Apple(float weight) {
+        super(weight);
+    }
+}
---
+++ src/Fruit.java
@@ -0,0 +1,12 @@
+public abstract class Fruit {
+
+    private float weight;
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+}
